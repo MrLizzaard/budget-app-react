f17ee9c54e85efe374ecd9ba44afa23c2b712720
MrLizzaard <dhk21512@gmail.com>
2022-01-14 18:58:05 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Stack } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap={2} className="mb-4">
+        <h1 className="me-auto">가계부</h1>
+        <Button variant="primary">예산 추가</Button>
+        <Button variant="outline-primary">지출 추가</Button>
+      </Stack>
+      <div></div>
+    </Container>
   );
 }
 
